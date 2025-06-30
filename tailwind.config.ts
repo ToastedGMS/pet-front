042e3153486c49ba6a78ea4ac023ab997f39212f
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-06-30 15:04:32 -0300

    Add ocean color to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				ocean: '#1F6D77',
+			},
+		},
 	},
 	plugins: [],
 };
