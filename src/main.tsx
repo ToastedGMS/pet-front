9d18ee113f1978c539a0466371677d1114e54962
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-06-30 13:20:08 -0300

    Refactor main.tsx to use Layout component and set up routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Layout from './pages/Layout.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<BrowserRouter>
+		<StrictMode>
+			<Routes>
+				<Route path="/" element={<Layout />}></Route>
+			</Routes>
+		</StrictMode>
+	</BrowserRouter>
+);
