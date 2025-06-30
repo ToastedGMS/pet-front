b1de96a52ac74a51b6dae769bb7ac1467327b597
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-06-30 15:05:26 -0300

    Add header inside Layout component  and correspondent tests

---
+++ src/pages/Layout.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Layout from './Layout';
+
+describe('Layout Component', () => {
+	beforeEach(() => {
+		render(<Layout />);
+	});
+	it('renders a header', () => {
+		expect(screen.getByRole('banner')).toBeInTheDocument();
+	});
+	it('header contains logo', () => {
+		expect(screen.getByRole('banner')).toContainElement(
+			screen.getByRole('img')
+		);
+	});
+	it('header contains a search bar', () => {
+		expect(screen.getByRole('banner')).toContainElement(
+			screen.getByRole('searchbox')
+		);
+	});
+	it('header contains an icon list', () => {
+		expect(screen.getByRole('banner')).toContainElement(
+			screen.getByTestId('icon-list')
+		);
+	});
+});
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,33 @@
 import type { JSX } from 'react';
+import { HiLogin, HiOutlineShoppingBag } from 'react-icons/hi';
 import { Outlet } from 'react-router-dom';
 
 export default function Layout(): JSX.Element {
 	return (
 		<>
+			<header className="flex items-center justify-between w-full mt-4 px-4 bg-ocean">
+				<img
+					src="logo-transparent.png"
+					alt="PetLove Logo"
+					className="w-40 hover:cursor-pointer"
+				/>
+				<input
+					className="border border-gray-300 rounded-2xl px-4 py-1 w-3/6"
+					type="search"
+					placeholder="Search..."
+				/>
+				<ul className="flex items-center gap-4" data-testid="icon-list">
+					<li>
+						<HiLogin
+							data-testid="login-icon"
+							className="text-3xl hover:cursor-pointer text-white hover:text-black transition-colors duration-300"
+						/>
+					</li>
+					<li>
+						<HiOutlineShoppingBag className="text-3xl hover:cursor-pointer text-white hover:text-black transition-colors duration-300" />
+					</li>
+				</ul>
+			</header>
 			<Outlet />
 		</>
 	);
