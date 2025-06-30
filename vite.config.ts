e647aae7ffa01ea4089590559ddd68ab83cd9ad7
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-06-30 13:41:57 -0300

    setup vitest and RTL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, type UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: { environment: 'jsdom', globals: true, setupFiles: './tests/setup.js' },
+} as UserConfig);
