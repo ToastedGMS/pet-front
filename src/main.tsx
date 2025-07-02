84188b887a5e11aed3e00e47a4598fdec5d80bc8
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-07-02 11:42:20 -0300

    Add routes for product categories

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,23 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Layout from './pages/Layout.tsx';
+import Dogs from './pages/Dogs/Dogs.tsx';
+import Cats from './pages/Cats/Cats.tsx';
+import Birds from './pages/Birds/Birds.tsx';
+import Sales from './pages/Sales/Sales.tsx';
+import Services from './pages/Services/Services.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
 		<StrictMode>
 			<Routes>
-				<Route path="/" element={<Layout />}></Route>
+				<Route path="/" element={<Layout />}>
+					<Route path="/dogs" element={<Dogs />} />
+					<Route path="/cats" element={<Cats />} />
+					<Route path="/birds" element={<Birds />} />
+					<Route path="/sales" element={<Sales />} />
+					<Route path="/services" element={<Services />} />
+				</Route>
 			</Routes>
 		</StrictMode>
 	</BrowserRouter>
