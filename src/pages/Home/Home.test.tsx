f44932dcf4d3fd62822c93777bf59742422b1ec9
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-07-03 13:35:20 -0300

    Remove heading from Home component

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -10,10 +10,7 @@ describe('Home', () => {
 			</MemoryRouter>
 		);
 	});
-	it('renders the Home component heading', () => {
-		const heading = screen.getByRole('heading', { level: 2 });
-		expect(heading).toBeInTheDocument();
-	});
+
 	it('renders the image carrousel correctly', () => {
 		const carousel = screen.getByTestId('home-carrousel');
 		expect(carousel).toBeInTheDocument();
