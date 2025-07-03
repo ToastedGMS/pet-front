3fcbec60878be494028281ef4dfa4084ca1ff5f7
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-07-03 16:45:28 -0300

    Add server configuration to Vite for local development

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: '0.0.0.0',
+	},
 	test: { environment: 'jsdom', globals: true, setupFiles: './tests/setup.js' },
 } as UserConfig);
