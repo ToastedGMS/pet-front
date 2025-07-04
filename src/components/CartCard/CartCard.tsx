2cc781c5f60db8150d357ca1d58fdeccd1d89534
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-07-04 10:30:42 -0300

    Refactor CartCard to integrate add and remove item functionality with CartContext

--- src/components/CartCard/CartCard.tsx
+++ src/components/CartCard/CartCard.tsx
@@ -1,21 +1,24 @@
 import { HiOutlineMinus, HiOutlinePlus } from 'react-icons/hi';
 import type { Product } from '../../types/Product';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
 
 interface CartCardProps {
 	cartItem: Product;
 }
 
 export default function CartCard({ cartItem }: CartCardProps) {
+	const { addItem, removeItem } = useContext(CartContext);
 	return (
 		<div data-testid="product-card">
 			<img src={cartItem.imageUrl} alt={cartItem.name} />
 			<h3>{cartItem.name}</h3>
 			<p>{cartItem.price}</p>
-			<button data-testid="add-btn">
-				<HiOutlinePlus />
-			</button>
-			<button data-testid="rmv-btn">
+			<button onClick={() => removeItem(cartItem)} data-testid="rmv-btn">
 				<HiOutlineMinus />
+			</button>
+			<button onClick={() => addItem(cartItem)} data-testid="add-btn">
+				<HiOutlinePlus />
 			</button>
 		</div>
 	);
